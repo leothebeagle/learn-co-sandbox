daf841d8ea975c89a95867a1a64722c6df8b2eac
Aziz Kosber <a.kosber@gmail.com>
2021-04-30 19:22:05 +0000

    Automatically backed up by Learn

---
+++ baby.rb
@@ -0,0 +1,17 @@
+class Baby
+  
+  attr_accessor :name
+  
+  def initialize(name = nil)
+    @name = name
+    puts "Waaaaaaaa!!!!!"
+  end
+  
+  def cry
+    puts "Waaaaaaaa!!!!!"
+  end
+  
+
+  
+  
+end
